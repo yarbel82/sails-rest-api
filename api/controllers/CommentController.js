2f070e63151d3f29cef9c3b309797f1c2a6e174b
Ubuntu <ubuntu@ip-172-31-15-154.eu-central-1.compute.internal>
2020-06-11 21:37:07 +0000

    test_new

--- api/controllers/CommentController.js
+++ api/controllers/CommentController.js
@@ -3,10 +3,8 @@
  *
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
- */
+*/
 
-module.exports = {
-  
-
+module.exports = {	
 };
 
